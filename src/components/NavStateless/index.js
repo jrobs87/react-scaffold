900313a3aa0155fb3379af66527bf954c1719202
John Robertson <tiktaalikdesign@gmail.com>
2019-11-15 02:02:07 -0500

    stateless nav started

---
+++ src/components/NavStateless/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import './index.css';
+
+export default class NavStateless extends Component {
+
+    handleClick() {
+        const wrapper = document.getElementById('wrapper');
+        wrapper.classList.toggle('is-nav-open');
+        console.log('clicked');
+    }
+
+    render() {
+        return (
+            <div className="wrapper">
+
+                <div className="nav-icon" onClick={() => this.handleClick()}>
+                    Navigation
+                </div>
+
+                <div className="nav__body" id="wrapper">
+                    <nav >
+                        <NavLink onClick={() => this.handleClick()} className="nav-link" activeClassName="nav-link-active" to='/' exact >Route - Home</NavLink>
+                        <NavLink onClick={() => this.handleClick()} className="nav-link" activeClassName="nav-link-active" to='/route-1'>Route - One</NavLink>
+                        <NavLink onClick={() => this.handleClick()} className="nav-link" activeClassName="nav-link-active" to='/route-2'>Route - Two</NavLink>
+                        <NavLink onClick={() => this.handleClick()} className="nav-link" activeClassName="nav-link-active" to='/route-api'>Route - API</NavLink>
+                        <NavLink onClick={() => this.handleClick()} className="nav-link" activeClassName="nav-link-active" to='/notfound'>Route - 404</NavLink>
+                        <NavLink onClick={() => this.handleClick()} className="nav-link" activeClassName="nav-link-active" to='/route-rtg'>Route - RTG</NavLink>
+                        <br />
+                        <NavLink onClick={() => this.handleClick()} className="nav-link" activeClassName="nav-link-active" to='/route-index'>Route - Index</NavLink>
+                    </nav>
+                </div>
+            </div>
+        )
+    }
+}
