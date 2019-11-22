0f9a1bf263bfadfb906592f435c3002bc0001630
John Robertson <tiktaalikdesign@gmail.com>
2019-11-21 23:05:45 -0500

    more nav fine tuning, adding styling

--- src/routes/1-home/index.js
+++ src/routes/1-home/index.js
@@ -1,32 +1,42 @@
 import React from "react";
 import { Container, Row, Col, Button } from "reactstrap";
 import { Link } from 'react-router-dom';
+import { Fade } from 'react-reveal';
+
+import img from '../../imgs/yoga.jpg'
 
 import './index.css';
 
+const style = {
+  backgroundImage: `url(${img})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'left'
+}
 
 // Homepage template.
 function RouteZero() {
   console.log("Rendered path '/' - RouteZero.");
 
   return (
-    <div className="page-wrapper">
-      <Container fluid={false}>
-        <Row>
-          <Col lg={6}>
-            <h1>Homepage</h1>
-            <h3>App Entry Point</h3>
-            <p>Root route for the application serving as the homepage.
+    <Fade duration={300} delay={200}>
+      <div className="page-wrapper" style={style}>
+        <Container fluid={false}>
+          <Row>
+            <Col lg={4} md={6}>
+              <h1>Homepage</h1>
+              <h3>App Entry Point</h3>
+              <p>Root route for the application serving as the homepage.
                 Change the Component name and config in the index.js file (found in the Router component).</p>
-            <Link to='/route-index'>
-              <Button className='btn'>
-                App Index
+              <Link to='/route-index'>
+                <Button className='btn'>
+                  App Index
                   </Button>
-            </Link>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+              </Link>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </Fade>
   );
 }
 
