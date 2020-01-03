bc71696d6e1371a2a5fb653385ec12df5d2a752e
John Robertson <tiktaalikdesign@gmail.com>
2020-01-02 23:15:46 -0500

    utter nonense and pure genius

---
+++ src/components/NavLinks/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import RouteZero from './routes/1-home';
+
+const NavArray = [
+    {
+        path: '/',
+        to: '',
+        name: 'Route - Home',
+        component: <RouteZero />
+    },
+    {
+        path: '/',
+        to: '',
+        name: 'Route - Home',
+        component: <RouteZero />
+    },
+    {
+        path: '/',
+        to: '',
+        name: 'Route - Home',
+        component: <RouteZero />
+    },
+    {
+        path: '/',
+        to: '',
+        name: 'Route - Home',
+        component: <RouteZero />
+    },
+    {
+        path: '/',
+        to: '',
+        name: 'Route - Home',
+        component: <RouteZero />
+    },
+    {
+        path: '/',
+        to: '',
+        name: 'Route - Home',
+        component: <RouteZero />
+    },
+    {
+        path: '/',
+        to: '',
+        name: 'Route - Home',
+        component: <RouteZero />
+    },
+]
+
+
+
+{/* <NavLink onClick={this.handleClick} className="nav-link" activeClassName="nav-link-active" to='/' exact >Route - Home</NavLink>
+<NavLink onClick={this.handleClick} className="nav-link" activeClassName="nav-link-active" to='/route-1'>Route - One</NavLink>
+<NavLink onClick={this.handleClick} className="nav-link" activeClassName="nav-link-active" to='/route-2'>Route - Two</NavLink>
+<NavLink onClick={this.handleClick} className="nav-link" activeClassName="nav-link-active" to='/route-api'>Route - API</NavLink>
+<NavLink onClick={this.handleClick} className="nav-link" activeClassName="nav-link-active" to='/notfound'>Route - 404</NavLink>
+<NavLink onClick={this.handleClick} className="nav-link" activeClassName="nav-link-active" to='/route-rtg'>Route - RTG</NavLink>
+<NavLink onClick={this.handleClick} className="nav-link" activeClassName="nav-link-active" to='/route-sandbox'>Route - Sandbox</NavLink>
+<NavLink onClick={this.handleClick} className="nav-link" activeClassName="nav-link-active" to='/route-sandbox-nav'>Route - Sandbox Nav</NavLink>
+<br />
+<NavLink onClick={this.handleClick} className="nav-link" activeClassName="nav-link-active" to='/route-index'>Route - Index</NavLink> */}
+
+export default NavArray
--- src/routes/8-pageSandbox/index.js
+++ src/routes/8-pageSandbox/index.js
@@ -27,50 +27,25 @@ export default class SandboxNav extends React.Component {
         console.log(`Component updated from render() with updated state of ${this.state.data}`)
     };
 
-    photos = [
-        {
-            src: img1,
-            width: 9,
-            height: 6
-        },
-        {
-            src: img2,
-            width: 1,
-            height: 1
-        },
-        {
-            src: img3,
-            width: 1,
-            height: 1
-        },
-        {
-            src: img4,
-            width: 1,
-            height: 1
-        },
-        {
-            src: img5,
-            width: 1,
-            height: 1
-        }
-    ];
-
     render() {
         return (
             <div className="wrapper">
                 {/* <Gallery photos={this.photos} lightbox /> */}
-                <div className="nav-trigger" onClick={this.load}>
-                    MENU
+                <div className={!this.state.data ? "nav-trigger" : "nav-trigger active"} onClick={this.load}>
+                <div className={!this.state.data ? "nav-trigger_text" : "nav-trigger_text active"}>
+                    <div className={!this.state.data ? "menu-open" : "menu-open active"}>MENU</div>
+                    <div className={!this.state.data ? "menu-close" : "menu-close active"}>CLOSE</div>
+                </div>
                 </div>
 
                 <div className={!this.state.data ? "nav-content" : "nav-content active"}>
 
                     <div className={!this.state.data ? "nav-elements" : "nav-elements active"}>
-                        <div onClick={this.load}>PHOTOGRAPHY</div>
-                        <p>Selected works from all projects spanning a decade.</p>
-                        <div onClick={this.load}>JOURNAL</div>
+                        <div onClick={this.load}>fotografía</div>
+                        <p>Selected works from all projects spanning a decade of human culture.</p>
+                        <div onClick={this.load}>diario</div>
                         <p>Musings, ramblings, and utter bullshit.</p>
-                        <div onClick={this.load}>A'MI</div>
+                        <div onClick={this.load}>i'MI</div>
                         <p>Talk about how baller I am at life.</p>
                     </div>
                 </div>
