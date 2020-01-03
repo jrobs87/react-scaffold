5ec4aebb4cb58c6f9b3e28e387b9a6256605b99b
John Robertson <tiktaalikdesign@gmail.com>
2020-01-02 19:17:53 -0500

    fixed jitter in transition moving from active

--- src/routes/8-pageSandbox/index.js
+++ src/routes/8-pageSandbox/index.js
@@ -58,28 +58,22 @@ export default class SandboxNav extends React.Component {
     render() {
         return (
             <div className="wrapper">
-                <Gallery photos={this.photos} lightbox />
+                {/* <Gallery photos={this.photos} lightbox /> */}
                 <div className="nav-trigger" onClick={this.load}>
-                    Click here to update state.
-                <button style={{ display: "block" }}> NAVIGATION state: {this.state.data.toString()}</button>
+                    MENU
                 </div>
 
                 <div className={!this.state.data ? "nav-content" : "nav-content active"}>
 
                     <div className={!this.state.data ? "nav-elements" : "nav-elements active"}>
-                        <div>PHOTOGRAPHY</div>
-                        <div>JOURNAL</div>
-                        <div>A'MI</div>
+                        <div onClick={this.load}>PHOTOGRAPHY</div>
+                        <p>Selected works from all projects spanning a decade.</p>
+                        <div onClick={this.load}>JOURNAL</div>
+                        <p>Musings, ramblings, and utter bullshit.</p>
+                        <div onClick={this.load}>A'MI</div>
+                        <p>Talk about how baller I am at life.</p>
                     </div>
-
-
-
                 </div>
-
-
-
-
-
             </div>
         )
     }
